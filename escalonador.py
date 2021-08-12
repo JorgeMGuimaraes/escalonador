dfbbd07b3fe09cc7bc41aae3bcd9064f5c8e68e7
Jorge Guimaraes <jmguimaraes@id.uff.br>
2021-08-12 08:22:08 -0300

    add: imprimir andamento dos processos

--- escalonador.py
+++ escalonador.py
@@ -34,14 +34,45 @@ def imprime_processos_recebidos(processos:  List[Processo]) -> None:
         tempo = 'tempo real' if processo.prioridade == 0 else 'usuário'
         disco = 'sem necessidade de recursos de E/S' if processo.io == 0 else f'requer { processo.io} unidade de disco'
         print(f'Processo {processo.id_processo}: chegada no momento {processo.chegada}, prioridade {processo.prioridade} ({tempo}), duração de {processo.duracao} segundos de CPU e memória de {processo.memoria} MBytes, {disco}')
+    return
 
-#sorted(s, key = lambda x: (x[1], x[2]))
+def imprime_andamento(quantum: int, recursos: Recursos) -> None:
+    espacamento = '    '
+    s  = 'Estado Atual:\n'
+    s += 'Recursos disponíveis:\n'
+    s += f'{espacamento}CPU:     {recursos.cpus}\n'
+    s += f'{espacamento}Discos:  {recursos.discos}\n'
+    s += f'{espacamento}MP:      {recursos.memoria}MB\n'
+    s += f'{espacamento}Quantum: {quantum}\n'
+    print(s)
+    return
+
 def main():
-    processos = ordena_processos(processa_entrada())
+    processos   = ordena_processos(processa_entrada())
+    recursos    = Recursos()
+    quantum     = 0
+
+    imprime_andamento(quantum, recursos)
     imprime_processos_recebidos(processos)
-    r = Recursos()
-    print(r.cpus)
+    return
 
 ## Programa principal
 if __name__ == "__main__": main()
 
+"""
+Estado Atual:
+Recursos disponíveis:
+    CPU:    4
+    Discos: 4
+    MP:     15246 MB
+    Quantum: 12
+Processos:
+    Novo:        Processo 0
+    Novo:        Processo 1
+    Processando: Processo 1
+Andamento:
+                |12|13|14|15|
+    Processo 0: |* | 
+    Processo 1: |  |
+
+"""
