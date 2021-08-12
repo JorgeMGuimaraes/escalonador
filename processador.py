cb263a2c4743de5570f2c45fa89b21eadf5364c8
Jorge Guimaraes <jmguimaraes@id.uff.br>
2021-08-12 13:44:10 -0300

    add: executar politca, nao testado

--- processador.py
+++ processador.py
@@ -11,8 +11,13 @@ class Processador:
     
     def adiciona_processo(self, processo: Processo) -> None:
         self.processos.append(processo)
-        print(f'Processo {processo.id_processo} foi atribuido ao Processador {self.id_processador}')
         return
 
     def define_processo_atual(self, processo: Processo) -> None:
         self.processo_atual = processo
+        print(f'Processo {processo.id_processo} foi atribuido ao Processador {self.id_processador}')
+        return
+
+    def executar(self, politica) -> None:
+        politica(self.processo_atual)
+        return
