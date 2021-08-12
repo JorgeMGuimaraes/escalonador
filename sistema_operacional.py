f00ea4524690d21456969ba72b7aae151aedbf3a
Jorge Guimaraes <jmguimaraes@id.uff.br>
2021-08-12 12:34:06 -0300

    add: escalonador

--- sistema_operacional.py
+++ sistema_operacional.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 ## imports
+from escalonador    import Escalonador
 from processo       import Processo
 from recursos       import Recursos
-
-from typing     import List
+from typing         import List
 ## classes
 
 ## definicoes
@@ -22,13 +22,13 @@ def processa_entrada() -> List[Processo]:
 def ordena_processos(processos: List[Processo]) -> List[Processo]:
     return sorted(processos, key = lambda proc: (proc.chegada, proc.prioridade))
 
-def imprime_processos_recebidos(processos:  List[Processo]) -> None:
-    for processo in processos:
-        tempo = 'tempo real' if processo.prioridade == 0 else 'usuário'
-        disco = 'sem necessidade de recursos de E/S' if processo.io == 0 else f'requer { processo.io} unidade de disco'
-        print(f'Processo {processo.id_processo}: chegada no momento {processo.chegada}, prioridade {processo.prioridade} ({tempo}), duração de {processo.duracao} segundos de CPU e memória de {processo.memoria} MBytes, {disco}')
-    print()
-    return
+# def imprime_processos_recebidos(processos:  List[Processo]) -> None:
+#     for processo in processos:
+#         tempo = 'tempo real' if processo.prioridade == 0 else 'usuário'
+#         disco = 'sem necessidade de recursos de E/S' if processo.io == 0 else f'requer { processo.io} unidade de disco'
+#         print(f'Processo {processo.id_processo}: chegada no momento {processo.chegada}, prioridade {processo.prioridade} ({tempo}), duração de {processo.duracao} segundos de CPU e memória de {processo.memoria} MBytes, {disco}')
+#     print()
+#     return
 
 def imprime_recursos(quantum: int, recursos: Recursos) -> None:
     espacamento = '    '
@@ -38,41 +38,43 @@ def imprime_recursos(quantum: int, recursos: Recursos) -> None:
     print(s)
     return
 
-def imprime_andamento(quanta:int, processos: List[Processo]) -> None:
-    espacamento = '    '
-    s = 'Andamento:\n'
-    s+= '                 |'
-    for quantum in range(quanta + 1):
-        s += f'{quantum:>2}|'
-    print(f'{s}\n')
+# def imprime_andamento(quanta:int, processos: List[Processo]) -> None:
+#     espacamento = '    '
+#     s = 'Andamento:\n'
+#     s+= '                 |'
+#     for quantum in range(quanta + 1):
+#         s += f'{quantum:>2}|'
+#     print(f'{s}\n')
 
-    for processo in processos:
-        s = f'{espacamento}Processo {processo.id_processo:>2}: |'
-        for processou in processo.andamento:
-            s += '* |' if processou else '  |'
-        print(f'{s}\n')
-    return
+#     for processo in processos:
+#         s = f'{espacamento}Processo {processo.id_processo:>2}: |'
+#         for processou in processo.andamento:
+#             s += '* |' if processou else '  |'
+#         print(f'{s}\n')
+#     return
 
-def continuar_processando(processos: List[Processo]) -> bool:
-    for processo in processos:
-        if processo.estado < 100: return True
+# def continuar_processando(processos: List[Processo]) -> bool:
+#     for processo in processos:
+#         if processo.estado < 100: return True
 
-    return False
+#     return False
 
 def main():
     cpus            = 4
     discos          = 4
     memoria         = 16 * 1024
     recursos        = Recursos(cpus, discos, memoria)
-    processos       = ordena_processos(processa_entrada())
+    escalonador     = Escalonador(processa_entrada())
+    #processos       = ordena_processos(processa_entrada())
     quanta          = 0
-    imprime_processos_recebidos(processos)
-    while continuar_processando(processos):
+    escalonador.imprime_processos_recebidos()
+    print()
+    while escalonador.continuar_processando():
         imprime_recursos(quanta, recursos)
         recursos.imprime_processadores()
         recursos.imprime_memoria()
         recursos.imprime_discos()
-        imprime_andamento(quanta, processos)
+        escalonador.imprime_andamento(quanta)
         quanta += 1
         _       = input()
     return
