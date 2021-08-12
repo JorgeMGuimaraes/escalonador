5c4b8b388fca70cf50e84553c68f8797649e8d70
Jorge Guimaraes <jmguimaraes@id.uff.br>
2021-08-12 11:29:40 -0300

    add: define processador

---
+++ processador.py
@@ -0,0 +1,18 @@
+## imports
+from processo import Processo
+
+## classes
+class Processador:
+    def __init__(self, id_processador) -> None:
+        self.processos      = []
+        self.processo_atual = None
+        self.id_processador = id_processador
+        return
+    
+    def adiciona_processo(self, processo: Processo) -> None:
+        self.processos.append(processo)
+        print(f'Processo {processo.id_processo} foi atribuido ao Processador {self.id_processador}')
+        return
+
+    def define_processo_atual(self, processo: Processo) -> None:
+        self.processo_atual = processo
