d47e0eb7215dc037fb3c38b2f12d289b79547298
Jorge Guimaraes <jmguimaraes@id.uff.br>
2021-08-13 08:21:31 -0300

    fix: clean up

--- processador.py
+++ processador.py
@@ -4,13 +4,8 @@ from processo import Processo
 ## classes
 class Processador:
     def __init__(self, id_processador) -> None:
-        self.processos      = []
         self.processo_atual = None
         self.id_processador = id_processador
-        return
-    
-    def adiciona_processo(self, processo: Processo) -> None:
-        self.processos.append(processo)
         return
 
     def define_processo_atual(self, processo: Processo) -> None:
