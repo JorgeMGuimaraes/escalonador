0c6ff24019c8c9371d44fd4051cea5626e5b778d
Jorge Guimaraes <jmguimaraes@id.uff.br>
2021-08-18 10:40:26 -0300

    fix: renomeia para thread

--- escalonador.py
+++ escalonador.py
@@ -9,90 +9,88 @@ from estado_processo import EstadoProcesso
 # TODO: fix: devolver memoria
 
 class Escalonador:
-    def __init__(self, recursos:Recursos, processos: List[Processo]) -> None:
-        self.processos: List[Processo]                      = self.ordena_processos(processos)
-        self.processos_nao_iniciados: List[Processo]        = []
-        self.processos_novos: List[Processo]                = []
-        self.processos_prontos: List[Processo]              = []
-        self.processos_prontos_suspenso: List[Processo]     = []
-        self.processos_executando: List[Processo]           = []
-        # TODO: ver so pode renomear para prontosuspenso
-        self.processos_suspensos: List[Processo]            = []
-        self.processos_bloqueados: List[Processo]           = []
-        self.processos_bloqueados_suspensos: List[Processo] = []
-        self.processos_finalizados: List[Processo]          = []
+    def __init__(self, recursos:Recursos, threads: List[Processo]) -> None:
+        self.threads: List[Processo]                      = self.ordena_processos(threads)
+        self.lista_nao_iniciados: List[Processo]        = []
+        self.lista_novos: List[Processo]                = []
+        self.lista_prontos: List[Processo]              = []
+        self.lista_prontos_suspenso: List[Processo]     = []
+        self.lista_executando: List[Processo]           = []
+        self.lista_bloqueados: List[Processo]           = []
+        self.lista_bloqueados_suspensos: List[Processo] = []
+        self.lista_finalizados: List[Processo]          = []
 
         self.executar_operacao = {
-            'Instanciar':                                   EstadoProcesso( atual=self.processos_nao_iniciados,
-                                                                            proximo=self.processos_novos,
+            'Instanciar':                                   EstadoProcesso( atual=self.lista_nao_iniciados,
+                                                                            proximo=self.lista_novos,
                                                                             estado=estado['novo'],
                                                                             msg_padrao='Novo processo instanciado em memória'),
 
-            'Admitir como pronto-suspenso':                 EstadoProcesso( atual=self.processos_novos,
-                                                                            proximo=self.processos_prontos_suspenso,
+            'Admitir como pronto-suspenso':                 EstadoProcesso( atual=self.lista_novos,
+                                                                            proximo=self.lista_prontos_suspenso,
                                                                             estado=estado['pronto_suspenso'],
                                                                             msg_padrao='Foi admitido como pronto-suspenso'),
 
-            'Admitir como pronto':                          EstadoProcesso( atual=self.processos_novos,
-                                                                            proximo=self.processos_prontos,
+            'Admitir como pronto':                          EstadoProcesso( atual=self.lista_novos,
+                                                                            proximo=self.lista_prontos,
                                                                             estado=estado['pronto'],
                                                                             msg_padrao='Foi admitido pronto'),
 
-            'Ativar pronto-suspenso para pronto':           EstadoProcesso( atual=self.processos_prontos_suspenso,
-                                                                            proximo=self.processos_prontos,
+            'Ativar pronto-suspenso para pronto':           EstadoProcesso( atual=self.lista_prontos_suspenso,
+                                                                            proximo=self.lista_prontos,
                                                                             estado=estado['pronto'],
                                                                             msg_padrao='Estava como pronto-suspenso e foi ativado. Está como pronto'),
 
-            'Suspender pronto para pronto-suspenso':        EstadoProcesso( atual=self.processos_prontos,
-                                                                            proximo=self.processos_prontos_suspenso,
+            'Suspender pronto para pronto-suspenso':        EstadoProcesso( atual=self.lista_prontos,
+                                                                            proximo=self.lista_prontos_suspenso,
                                                                             estado=estado['pronto_suspenso'],
                                                                             msg_padrao='Foi suspenso para o estado pronto-suspenso'),
 
-            'Despachar pronto para executando':             EstadoProcesso( atual=self.processos_prontos,
-                                                                            proximo=self.processos_executando,
+            'Despachar pronto para executando':             EstadoProcesso( atual=self.lista_prontos,
+                                                                            proximo=self.lista_executando,
                                                                             estado=estado['executando'],
                                                                             msg_padrao='Foi despachado para execução'),
 
-            'Pausar executando para pronto':                EstadoProcesso( atual=self.processos_executando,
-                                                                            proximo=self.processos_prontos,
+            'Pausar executando para pronto':                EstadoProcesso( atual=self.lista_executando,
+                                                                            proximo=self.lista_prontos,
                                                                             estado=estado['pronto'],
                                                                             msg_padrao='Estava executando e perdeu processador. Foi pausado e está no estado pronto'),
 
-            'Bloqueia executando':                          EstadoProcesso( atual=self.processos_executando,
-                                                                            proximo=self.processos_bloqueados,
+            'Bloqueia executando':                          EstadoProcesso( atual=self.lista_executando,
+                                                                            proximo=self.lista_bloqueados,
                                                                             estado=estado['bloqueado'],
                                                                             msg_padrao='Estava executando mas precisa ler/gravar. Foi bloqueado a espera de ocorrer evento'),
 
-            'Evento ocorre, e vai para pronto':              EstadoProcesso( atual=self.processos_bloqueados,
-                                                                            proximo=self.processos_prontos,
+            'Evento ocorre, e vai para pronto':              EstadoProcesso( atual=self.lista_bloqueados,
+                                                                            proximo=self.lista_prontos,
                                                                             estado=estado['pronto'],
                                                                             msg_padrao='Evento de leitura/escrita ocorreu e vai para pronto'),
 
-            'Suspende bloqueado para bloqueado-suspenso':   EstadoProcesso( atual=self.processos_bloqueados,
-                                                                            proximo=self.processos_bloqueados_suspensos,
+            'Suspende bloqueado para bloqueado-suspenso':   EstadoProcesso( atual=self.lista_bloqueados,
+                                                                            proximo=self.lista_bloqueados_suspensos,
                                                                             estado=estado['bloqueado_suspenso'],
                                                                             msg_padrao='Não há discos disponíveis, Suspende bloqueado para bloqueado-suspenso'),
 
-            'Ativa suspenso-bloqueado para bloquado':       EstadoProcesso( atual=self.processos_bloqueados_suspensos,
-                                                                            proximo=self.processos_bloqueados,
+            'Ativa suspenso-bloqueado para bloquado':       EstadoProcesso( atual=self.lista_bloqueados_suspensos,
+                                                                            proximo=self.lista_bloqueados,
                                                                             estado=estado['bloqueado'],
                                                                             msg_padrao='Há discos disponíveis. Ativa suspenso-bloqueado para pronto-suspenso'),
 
-            'Evento ocorre, e vai para pronto-suspenso':    EstadoProcesso( atual=self.processos_bloqueados_suspensos,
-                                                                            proximo=self.processos_prontos_suspenso,
+            'Evento ocorre, e vai para pronto-suspenso':    EstadoProcesso( atual=self.lista_bloqueados_suspensos,
+                                                                            proximo=self.lista_prontos_suspenso,
                                                                             estado=estado['pronto_suspenso'],
                                                                             msg_padrao='Evento ocorre, e vai para pronto-suspenso'),
 
-            'Libera executando para finalizado':            EstadoProcesso( atual=self.processos_executando,
-                                                                            proximo=self.processos_finalizados,
+            'Libera executando para finalizado':            EstadoProcesso( atual=self.lista_executando,
+                                                                            proximo=self.lista_finalizados,
                                                                             estado=estado['finalizado'],
                                                                             msg_padrao='Processo terminou')
 
         }
         self.logs: List[str]                                = []
 
-        for processo in self.processos:
-            self.processos_nao_iniciados.append(processo)
+        for processo in self.threads:
+            self.lista_nao_iniciados.append(processo)
 
         self.recursos                                       = recursos
         # TODO: remover mgs desnecessarias
@@ -115,27 +113,27 @@ class Escalonador:
 
     ## usa
     def continuar_processando(self) -> bool:
-        for processo in self.processos:
-            if processo.estado < 100: return True
+        for thread in self.threads:
+            if thread.estado < 100: return True
 
         return False
    
     def ativa_prontos_suspensos(self) -> None:
-        for processo in self.processos_prontos_suspenso:
+        for processo in self.lista_prontos_suspenso:
             if self.recursos.ha_memoria_disponivel():
                 feedback_operacao = self.executar_operacao['Ativar pronto-suspenso para pronto'].neste(processo)
                 self.adiciona_ao_log(feedback_operacao)
         return
 
     def reserva_discos(self) -> None:
-        for processo in self.processos_bloqueados_suspensos:
+        for processo in self.lista_bloqueados_suspensos:
             if self.recursos.ha_discos_disponiveis():
                 for disco in self.recursos.discos:
                     if disco.pode_agregar(processo):
                         feedback_operacao = self.executar_operacao['Ativa suspenso-bloqueado para bloquado'].neste(processo)
                         self.adiciona_ao_log(feedback_operacao)
 
-        for processo in self.processos_bloqueados:
+        for processo in self.lista_bloqueados:
             if self.recursos.ha_discos_disponiveis():
                 for disco in self.recursos.discos:
                     disco.pode_agregar(processo)
@@ -166,14 +164,14 @@ class Escalonador:
         return
 
     def instanciar_processos(self, contador_quanta: int) -> None:
-        for processo in self.processos_nao_iniciados:
+        for processo in self.lista_nao_iniciados:
             if processo.chegada == contador_quanta:
                 feedback_operacao = self.executar_operacao['Instanciar'].neste(processo)
                 self.adiciona_ao_log(feedback_operacao)
         return
 
     def admitir_processos(self) -> None:
-        for processo in self.processos_novos:
+        for processo in self.lista_novos:
             if self.recursos.ha_memoria_disponivel(processo):
                 self.recursos.usa_memoria(processo.memoria)
                 feedback_operacao = self.executar_operacao['Admitir como pronto'].neste(processo)
@@ -184,7 +182,7 @@ class Escalonador:
         return
     
     def despachar(self) -> None:
-        for processo in self.processos_prontos:
+        for processo in self.lista_prontos:
             for processador in self.recursos.processadores:
                 if processador.pode_agregar(processo):
                     feedback_operacao = self.executar_operacao['Despachar pronto para executando'].neste(processo)
@@ -199,7 +197,7 @@ class Escalonador:
             if processador.pode_executar():
                 self.logs.append(f'Processo {processador.processo_atual.id_processo}: Executou no Core {processador.id_processador}')
 
-        for processo in self.processos_executando:
+        for processo in self.lista_executando:
             #continua executando
             if processo.estado == estado['executando']:
                 continue
@@ -225,11 +223,11 @@ class Escalonador:
         return
 
     def atualiza_estado_idle(self, quantum: int) -> None:
-        for processo in self.processos:
-            if processo in self.processos_executando:
+        for thread in self.threads:
+            if thread in self.lista_executando:
                 continue
-            if len(processo.andamento) <= quantum:
-                processo.aguardando()
+            if len(thread.andamento) <= quantum:
+                thread.aguardando()
         return
 
     def adiciona_ao_log(self, feedback: str) -> None:
@@ -238,10 +236,10 @@ class Escalonador:
 
     ## exibe
     def imprime_processos_recebidos(self) -> None:
-        for processo in self.processos:
-            tempo = 'tempo real' if processo.prioridade == 0 else 'usuário'
-            discos = 'sem necessidade de recursos de E/S' if processo.discos == 0 else f'requer { processo.discos} unidade(s) de disco(s)'
-            print(f'Processo {processo.id_processo}: chegada no momento {processo.chegada}, prioridade {processo.prioridade} ({tempo}), duração de {processo.duracao} segundos de CPU e memória de {processo.memoria} MBytes, {discos} disco(s)')
+        for thread in self.threads:
+            tempo = 'tempo real' if thread.prioridade == 0 else 'usuário'
+            discos = 'sem necessidade de recursos de E/S' if thread.discos == 0 else f'requer { thread.discos} unidade(s) de disco(s)'
+            print(f'Processo {thread.id_processo}: chegada no momento {thread.chegada}, prioridade {thread.prioridade} ({tempo}), duração de {thread.duracao} segundos de CPU e memória de {thread.memoria} MBytes, {discos} disco(s)')
         return
 
     def imprime_andamento(self, quanta:int) -> None:
@@ -252,9 +250,9 @@ class Escalonador:
             s += f'{quantum:>2}|'
         print(f'{s}\n')
 
-        for processo in self.processos:
-            s = f'{espacamento}Processo {processo.id_processo:>2}: |'
-            for processou in processo.andamento:
+        for thread in self.threads:
+            s = f'{espacamento}Processo {thread.id_processo:>2}: |'
+            for processou in thread.andamento:
                 s += '* |' if processou else '  |'
             print(f'{s}\n')
         return
